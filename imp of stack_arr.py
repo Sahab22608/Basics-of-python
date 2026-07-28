d9fc1afdb119ff2826a882ea28d289b66d8aa97a
Sahab22608 <sahabdeen22608@gmail.com>
2026-07-28 16:05:12 +0530

    Add files via upload

--- imp of stack_arr.py
+++ imp of stack_arr.py
@@ -8,38 +8,27 @@ class stack:
         self.top=None
     def is_empty(self):
         return self.top is None
-
     def push(self,data):
         new_node=Node(data)
         new_node.next=self.top
         self.top=new_node
-
     def pop(self):
         if self.is_empty():
             return None
-        poped_data=self.top.data
+        popped_data=self.top.data
         self.top=self.top.next
-        return poped_data
-
+        return popped_data
     def peek(self):
         if self.is_empty():
             return None
         return self.top.data
+
 stack=stack()
-stack.push(1)
-stack.push(2)
-stack.push(3)
-
-print("Peek:",stack.peek())
-
-print("pop:",stack.pop())
-print("pop:",stack.pop())
-
-print("Peek:",stack.peek())
-
-   
-print("pop:",stack.pop())
-print("pop:",stack.pop())
-
-print("Is Empty:",stack.is_empty())
- 
+n=int(input("Enter the number:"))
+for i in range(n):
+    value=int(input("Enter element{i+1}:"))
+    stack.push(value)
+print("peek:",stack.peek())
+while not stack.is_empty():
+    print("pop:",stack.pop())
+    print("Is Empty:",stack.is_empty())
